d6c3beaace42d2542e475728e7578d75626bbb92
Paul Milne <hallhill@gmail.com>
2016-10-18 19:42:43 +0100

    adding a new shoes table to the shoes database

--- db/shoe_shop.sql
+++ db/shoe_shop.sql
@@ -1,9 +1,15 @@
 DROP TABLE shoes;
 
-CREATE TABLE shoes (
+CREATE TABLE orders (
   id SERIAL4 PRIMARY KEY,
   name VARCHAR(255),
   address VARCHAR(255),
   quantity INT2,
-  size VARCHAR(255)
 );
+
+CREATE TABLE shoes (
+    id SERIAL4 PRIMARY KEY,
+    models VARCHAR(255),
+    sizes INT2,
+    colors VARCHAR(255)
+    );
